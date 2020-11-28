6446f2e2a2db0dda3e460c278ad6040a18d58eac
vivekpisal <54746512+vivekpisal@users.noreply.github.com>
2020-11-28 23:06:41 +0530

    Add files via upload

--- dstructure/Graph.py
+++ dstructure/Graph.py
@@ -3,6 +3,9 @@ class Graph:
 		self.edges={}
 
 	def insert(self,x,y):
+		"""
+		x,y: Insert the nodes using x,y
+		"""
 		try:
 			if self.edges[x] or self.edges[y]:
 				self.edges[x].add(y)
@@ -22,6 +25,9 @@ class Graph:
 
 
 	def show(self):
+		"""
+		Print the whole graph
+		"""
 		for i in self.edges:
 			print(i,"-> ",end="")
 			for j in self.edges[i]:
@@ -41,6 +47,10 @@ class Graph:
 
 
 	def get_bfs(self,node):
+		"""
+		node: node represents the the node from which we want to 
+		start the bfs.
+		"""
 		queue=[node]
 		visited={x:False for x in self.edges}
 		visited[node]=True
@@ -55,3 +65,28 @@ class Graph:
 		return bfs
 
 
+	def get_dfs(self,node):
+		"""
+		node: node represents the the node from which we want to 
+		start the dfs.
+		"""
+		stack=[node]
+		dfs=[]
+		visited={i:False for i in self.edges}
+		visited[node]=True
+		while(len(stack)>0):
+			node=stack.pop(-1)
+			dfs.append(node)
+			for i in self.edges[node]:
+				if(visited[i]==False):
+					stack.append(i)
+					visited[i]=True
+		return dfs
+
+
+a=Graph()
+a.insert(1,2)
+a.insert(1,3)
+a.insert(2,3)
+print(a.get_bfs(1))
+print(a.get_dfs(1))
